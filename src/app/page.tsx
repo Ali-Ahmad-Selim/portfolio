1213ca828d009cba6bf1250af39dd26ce2de8c65
Ali Ahmad <ali732152ahmad@gmail.com>
2025-07-15 20:44:46 +0500

    structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Features from './comonents/Features'
+import Services from './comonents/Services'
+import Footer from './comonents/Footer'
+import Head from './comonents/Head'
+import Previous from './comonents/Previous'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <Head />
+      <Previous />
+      <Features />
+      <Services />
+      <Footer />
+    </div>
   )
 }
 
